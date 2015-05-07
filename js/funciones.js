6c64cec1a23dd26b3c942aa304e5e4ccf78484e8
RaquelDH17 <raquel_170298@hotmail.com>
2015-05-06 22:34:24 -0500

    Agrega
    Agregamos los archivos de jquery mobil

---
+++ js/funciones.js
@@ -0,0 +1,48 @@
+// JavaScript Document
+$(document).on("pagecreate","#Historia",function(){
+		$("#ami").on("swipe",function(){
+		$("#s1").text("Alexia, Alberto, Jaqueline, Angel & Raquel");
+		});
+});
+
+$(document).on("pagecreate","#Amigos",function(){
+		$("#a").on("swipe",function(){
+		$("#s2").text("Alexia Ceron, 17 años, Feliz");
+		});
+});
+
+$(document).on("pagecreate","#Amigos",function(){
+		$("#b").on("swipe",function(){
+		$("#s3").text("Jaqueline Lopez, 17 años, Sincera");
+		});
+});
+
+$(document).on("pagecreate","#Amigos",function(){
+		$("#c").on("swipe",function(){
+		$("#s4").text("Raquel Daza, 17 años, Alegre");
+		});
+});
+
+$(document).on("pagecreate","#Amigos",function(){
+		$("#d").on("swipe",function(){
+		$("#s5").text("Angel Torres, 17 años, No se XD");
+		});
+});
+
+$(document).on("pagecreate","#Amigos",function(){
+		$("#e").on("swipe",function(){
+		$("#s6").text("Alberto Camacho, 17 años, Timido");
+		});
+});
+
+$(document).on("pagecreate","#ayuda",function(){
+		$("#ay").on("swipe",function(){
+		$("#s7").text("Obtener ayuda!!");
+		});
+});
+
+$(document).on("pagecreate","#contacto",function(){
+		$("#con").on("swipe",function(){
+		$("#s8").text("Podras contactarnos en raquel_170298@hotmail.com o al numero 2221073394");
+		});
+});
